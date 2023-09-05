4207c94f859d10049a3b66ae758f95bbfbef7a7b
kmutsolgov <kmutsolgov@gmail.com>
2023-09-05 18:51:28 -0400

    first-test

---
+++ tests/auth.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Authentication & Authorization', () => {
+    test('Sigh in with existing credentials', async ({page}) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('kmutsolgov@gmail.com')
+        await page.locator('#normal_login_password').fill('eAvevMhHwzuHgZ9')
+        await page.locator('button[type="submit"]').click()
+
+        await expect(page.locator('.ant-avatar-square')).toBeVisible()
+    })
+    test('Sigh in with wrong email', async ({page}) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('test@gmail.com')
+        await page.locator('#normal_login_password').fill('eAvevMhHwzuHgZ9')
+        await page.locator('button[type="submit"]').click()
+
+        await expect(page.getByText('User login. Fail')).toBeVisible()
+    })
+})
